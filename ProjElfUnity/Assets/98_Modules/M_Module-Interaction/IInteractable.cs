2cf9bb5f00582e84f7a05e280a47d28d783c62ad
JulienCarrillo <xxwaxor13xx@gmail.com>
2020-11-24 19:24:14 +0100

    Module d'interaction

---
+++ ProjElfUnity/Assets/98_Modules/M_Module-Interaction/IInteractable.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace ProjElf.Interaction
+{
+    
+    public interface IInteractable 
+    {
+        GameObject gameObject { get; } // on ne peut pas mettre d'attribut dans une interface. du coup on doit la get 
+        void DoInteraction();
+        void StartBeingWatched();
+        void StopBeingWatched();
+    }
+    
+}
