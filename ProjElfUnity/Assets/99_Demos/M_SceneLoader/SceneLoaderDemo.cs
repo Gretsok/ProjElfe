74de00fdc8abafa36d06b26c349ac6625a1d0f15
JulienCarrillo <xxwaxor13xx@gmail.com>
2020-12-01 19:08:52 +0100

    Module SceneLoader

---
+++ ProjElfUnity/Assets/99_Demos/M_SceneLoader/SceneLoaderDemo.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ProjElf.SceneData;
+
+public class SceneLoaderDemo : MonoBehaviour 
+{
+    [SerializeField]
+    [Tooltip("scenData to load")]
+    SceneData scene = null;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("scene :" + scene.LevelName);
+            scene.LoadLevel();
+        }
+    }
+}
