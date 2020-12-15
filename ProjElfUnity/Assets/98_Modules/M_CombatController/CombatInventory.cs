59cec197252902e498f09f12b9dae1407965ffa4
JulienBergeon <55741381+JulienBergeon@users.noreply.github.com>
2020-12-15 18:38:03 +0100

    Changement d'arme (Pre test)

--- ProjElfUnity/Assets/98_Modules/M_CombatController/CombatInventory.cs
+++ ProjElfUnity/Assets/98_Modules/M_CombatController/CombatInventory.cs
@@ -12,10 +12,15 @@ namespace ProjElf.CombatController
         private Bow m_bow;
         //private AWeapon[] m_weapons = new AWeapon[3];
         private AWeapon selectedWeapon;
-        //Ref transform
+        //Ref transform quand équipé
         [SerializeField] private Transform posMelee;
         [SerializeField] private Transform posGrimoire;
         [SerializeField] private Transform posBow;
+        //Ref transform quand rangé
+        [SerializeField] private Transform posMeleeEquip;
+        [SerializeField] private Transform posGrimoireEquip;
+        [SerializeField] private Transform posBowEquip;
+
 
         // Start is called before the first frame update
         void Start()
@@ -27,6 +32,17 @@ namespace ProjElf.CombatController
         void Update()
         {
 
+        }
+        /// <summary>
+        /// Change le parent et la position de l'arme.
+        /// </summary>
+        /// <param name="changePosOf"></param>
+        /// <param name="wantedPos"></param>
+        private void ChangeWeaponTransform(AWeapon changePosOf, Transform wantedPos)
+        {
+            changePosOf.transform.SetParent(wantedPos); //On change le parent
+            changePosOf.transform.localPosition = Vector3.zero; //Position à 0
+            changePosOf.transform.localRotation = Quaternion.identity; //Rotation à 0
         }
 
         public void ChangeMeleeWeapon(MeleeWeaponData newMeleeWeaponData)
@@ -64,18 +80,35 @@ namespace ProjElf.CombatController
             //On part sur Epee / Grimoire / Arc donc :
             if((selectedWeapon is MeleeWeapon) && (m_grimoire != null))
             {
+                //On range le melee
+                ChangeWeaponTransform(m_meleeWeapon, posMelee);
+                //On sort le grimoire
+                ChangeWeaponTransform(m_grimoire, posGrimoireEquip);
                 selectedWeapon = m_grimoire;
             }
             else
             {
                 if((selectedWeapon is Grimoire) && (m_bow != null))
                 {
+                    //On range le grimoire
+                    ChangeWeaponTransform(m_grimoire, posGrimoire);
+                    //On sort le bow
+                    ChangeWeaponTransform(m_bow, posBowEquip);
                     selectedWeapon = m_bow;
                 }
                 else
                 {
-                    if(m_meleeWeapon != null)
+                    if((selectedWeapon is Bow) && (m_meleeWeapon != null))
                     {
+                        //On range le bow
+                        ChangeWeaponTransform(m_bow, posBow);
+                        //On sort le melee
+                        ChangeWeaponTransform(m_meleeWeapon, posMeleeEquip);
+                        selectedWeapon = m_meleeWeapon;
+                    }
+                    else //cas où on a pas d'arme, on équipe melee
+                    {
+                        ChangeWeaponTransform(m_meleeWeapon, posMeleeEquip);
                         selectedWeapon = m_meleeWeapon;
                     }
                 }
@@ -85,19 +118,36 @@ namespace ProjElf.CombatController
         {
             if((selectedWeapon is Bow) && (m_grimoire != null))
             {
+                //On range le bow
+                ChangeWeaponTransform(m_bow,posBow);
+                //On sort le grimoire
+                ChangeWeaponTransform(m_grimoire, posGrimoireEquip);
                 selectedWeapon = m_grimoire;
             }
             else
             {
                 if((selectedWeapon is Grimoire) && (m_meleeWeapon != null))
                 {
+                    //On range le grimoire
+                    ChangeWeaponTransform(m_grimoire, posGrimoire);
+                    //On sort le melee
+                    ChangeWeaponTransform(m_meleeWeapon, posMeleeEquip);
                     selectedWeapon = m_meleeWeapon;
                 }
                 else
                 {
-                    if (m_bow != null)
+                    if ((selectedWeapon is MeleeWeapon) && (m_bow != null))
                     {
+                        //On range le melee
+                        ChangeWeaponTransform(m_meleeWeapon, posMelee);
+                        //On sort le bow
+                        ChangeWeaponTransform(m_bow, posBowEquip);
                         selectedWeapon = m_bow;
+                    }
+                    else //cas où on a pas d'arme, on équipe melee
+                    {
+                        ChangeWeaponTransform(m_meleeWeapon, posMeleeEquip);
+                        selectedWeapon = m_meleeWeapon;
                     }
                 }
             }
