1209d090b695649e50a20207869e396df454933e
JulienCarrillo <56301168+JulienCarrillo@users.noreply.github.com>
2020-12-19 16:00:48 +0100

    Module animaux § starwars

---
+++ ProjElfUnity/Assets/98_Modules/M_GestionAnimaux/AnimalsManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public  class SavedAnimalData
+{
+    
+    public AnimalData AnimalData; 
+    public int Amount;
+
+    //1 
+}
+public class AnimalsManager : MonoBehaviour
+{
+    // ajouter une methode saveAnamal(AnimalData saveAnimal)
+        //check dans la liste SaveAnimal si il y a un saveAnimal 
+    //j'instentie car savedAnimalData ne depend pas ni de monoBehaviour ni de Scriptable objet.
+    private List<SavedAnimalData> m_savedAnimals = new List<SavedAnimalData>();
+    [SerializeField]
+    private AllAnimalsData m_allAnimalsData = null;
+    public List<SavedAnimalData> SavedAnimals => m_savedAnimals;
+
+    public AnimalData GetRandomAnimal()
+    {
+        //retourne AniamalData generer aleatoirement 
+        System.Random sysRand = new System.Random(); 
+        UnityEngine.Random.InitState(sysRand.Next(0, 100000)); //1ere generation aleatoire
+        int rand = UnityEngine.Random.Range(0,m_allAnimalsData.Animals.Length);//2nd generation aleatoire
+        AnimalData Animal = m_allAnimalsData.Animals[rand];
+        return Animal;
+    }
+                            
+}
---
+++ ProjElfUnity/Assets/99_Demos/StarBob/cameraMove.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraMove : MonoBehaviour
+{
+    public Vector3[] Position;
+    public float Speed = 1.0f;
+
+    private int m_currentIndex = 0 ;
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 currentPos = default;
+        transform.position = Vector3.Lerp(transform.position, currentPos, Speed * Time.deltaTime);
+
+    }
+}
