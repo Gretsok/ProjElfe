766cd153a5dd583f658a5993952ff24d331064f4
Gretsok <47755843+Gretsok@users.noreply.github.com>
2021-01-19 19:18:44 +0100

    Début WeaponSight

--- ProjElfUnity/Assets/98_Modules/M_CombatController/Bow.cs
+++ ProjElfUnity/Assets/98_Modules/M_CombatController/Bow.cs
@@ -13,7 +13,8 @@ namespace ProjElf.CombatController
         [SerializeField] private Arrow m_projectilePrefab;
         [SerializeField] private Transform posArrow;
         private CombatController m_owner;
-        
+
+        public Transform PosArrow => posArrow;
 
         public void InitBow(BowData.BowSaveData bowToInit)
         {
@@ -33,7 +34,7 @@ namespace ProjElf.CombatController
         public Arrow InstantiateInitializedArrow(Vector3 direction)//A transformer en void
         {
             Arrow newArrow;
-            newArrow = Instantiate<Arrow>(m_projectilePrefab, posArrow.position, posArrow.rotation);//instancie dans la scene du bow | transform = "position" du bow mais enfant
+            newArrow = Instantiate<Arrow>(m_projectilePrefab, posArrow.position, Quaternion.identity);//instancie dans la scene du bow | transform = "position" du bow mais enfant
             newArrow.transform.LookAt(posArrow.position + direction);//Peut etre à changer (à mettre dans arrow ptetre) //Change la rotation de la fleche dans la direction visée
             newArrow.InitArrow(m_owner, m_projectileSpeed, m_projectileRange, m_projectileDivingRate, m_damage);
             return newArrow;
