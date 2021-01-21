aa3199cd3dcc391e6ee30feb9c4881101dbc5ccc
Gretsok <47755843+Gretsok@users.noreply.github.com>
2021-01-21 22:28:04 +0100

    Test addressables

---
+++ ProjElfUnity/Assets/0_Users/Fergal/AddressablesTest/LoadingAddressablesTest.cs
@@ -0,0 +1,28 @@
+﻿using ProjElf.CombatController;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+public class LoadingAddressablesTest : MonoBehaviour
+{
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Addressables.LoadAssetsAsync<AWeaponData>("bow", null).Completed += obj =>
+        {
+            foreach (AWeaponData weaponData in obj.Result)
+            {
+                Instantiate(weaponData.WeaponPrefab);
+            }
+        };
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
