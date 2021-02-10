968c4a6860becc792bd31dfff6c6ab795d82d762
Gretsok <47755843+Gretsok@users.noreply.github.com>
2021-02-10 14:56:13 +0100

    avancée IK animator

--- ProjElfUnity/Assets/0_Users/Fergal/ProceduraleAnimations/IKAnimator.cs
+++ ProjElfUnity/Assets/0_Users/Fergal/ProceduraleAnimations/IKAnimator.cs
@@ -22,7 +22,7 @@ public class IKAnimator : MonoBehaviour
     [SerializeField]
     private float m_minDistanceToStep = 0.35f;
     [SerializeField]
-    private float m_stepDistance = 0.75f;
+    private float m_stepMultiplier = 0.75f;
 
     [SerializeField]
     private float m_footMovementSmoothValue = 0.2f;
@@ -54,67 +54,64 @@ public class IKAnimator : MonoBehaviour
     void MoveFoot()
     {
         m_controllerPositionOnGround = transform.position;
-        if(Physics.Raycast(transform.position, Vector3.down, out RaycastHit hitInfos, 5f))
+        if(Physics.Raycast(transform.position, Vector3.down, out RaycastHit hitInfos, 2f))
         {
             m_controllerPositionOnGround = hitInfos.point;
         }
-        Vector3 relativeFootCenterToPosition = transform.InverseTransformDirection(
-            (m_controllerPositionOnGround - Vector3.Lerp(m_leftFootTarget.position, m_rightFootTarget.position, 0.5f)));
-        if (relativeFootCenterToPosition.magnitude > m_minDistanceToStep)
+        Vector3 footCenterToPosition =
+            m_controllerPositionOnGround - Vector3.Lerp(m_leftFootTarget.position, m_rightFootTarget.position, 0.5f);
+        if (footCenterToPosition.magnitude > m_minDistanceToStep)
         {
             Vector3 rightFootToPos = (m_controllerPositionOnGround - m_rightFootTarget.position);
             Vector3 leftFootToPos = (m_controllerPositionOnGround - m_leftFootTarget.position);
+            bool canMoveRightFoot = false, canMoveLeftFoot = false;
 
-            if (Mathf.Abs(relativeFootCenterToPosition.z) > Mathf.Abs(relativeFootCenterToPosition.x))
+            if(Vector3.Angle(footCenterToPosition, rightFootToPos) < 90)
             {
-                if (Mathf.Sign(relativeFootCenterToPosition.z) * transform.InverseTransformPoint(m_rightFootTarget.position).z >
-                Mathf.Sign(relativeFootCenterToPosition.z) * transform.InverseTransformPoint(m_leftFootTarget.position).z)
+                canMoveRightFoot = true;
+            }
+            if(Vector3.Angle(footCenterToPosition, leftFootToPos) < 90)
+            {
+                canMoveLeftFoot = true;
+            }
+
+            if(canMoveRightFoot && canMoveLeftFoot)
+            {
+                //canMoveLeftFoot = false;
+                if(rightFootToPos.sqrMagnitude > leftFootToPos.sqrMagnitude)
                 {
-                    Debug.Log("MOVE LEFT");
-                    /*Vector3 newFootPosDirection = new Vector3(rightFootToPos.x,
-                        leftFootToPos.y,
-                        leftFootToPos.z).normalized * m_stepDistance;*/
-                    Vector3 newFootPosDirection = transform.InverseTransformDirection(
-                        new Vector3(-0.2f,
-                        0,
-                        m_stepDistance * Mathf.Sign(relativeFootCenterToPosition.z))
-                        );
-                    if (Physics.Raycast(m_controllerPositionOnGround + newFootPosDirection + Vector3.up * 1.5f,
-                        Vector3.down,
-                        out RaycastHit hitInfo,
-                        10f))
-                    {
-                        m_targetLeftFootPosition = hitInfo.point;
-                    }
+                    canMoveLeftFoot = false;
                 }
                 else
                 {
-                    Debug.Log("MOVE RIGHT");
-                    /*Vector3 newFootPosDirection = new Vector3(leftFootToPos.x,
-                        rightFootToPos.y,
-                        rightFootToPos.z).normalized * m_stepDistance;*/
-                    Vector3 newFootPosDirection = transform.InverseTransformDirection(
-                        new Vector3(0.2f,
-                        0,
-                        m_stepDistance * Mathf.Sign(relativeFootCenterToPosition.z))
-                        );
-                    if (Physics.Raycast(m_controllerPositionOnGround + newFootPosDirection + Vector3.up * 1.5f,
-                        Vector3.down,
-                        out RaycastHit hitInfo,
-                        10f))
-                    {
-                        m_targetRightFootPosition = hitInfo.point;
-                    }
+                    canMoveRightFoot = false;
                 }
             }
-            else
-            {
 
-            }            
+
+            if(canMoveRightFoot)
+            {
+                Vector3 newFootPosition = m_rightFootTarget.position +
+                    footCenterToPosition.normalized * rightFootToPos.magnitude * m_stepMultiplier;
+                while(!MoveRightFoot(newFootPosition))
+                {
+                    newFootPosition += (m_rightFootTarget.position - newFootPosition).normalized * 0.02f;
+                }
+            }
+            if(canMoveLeftFoot)
+            {
+                Vector3 newFootPosition = m_leftFootTarget.position +
+                    footCenterToPosition.normalized * leftFootToPos.magnitude * m_stepMultiplier;
+                while(!MoveLeftFoot(newFootPosition))
+                {
+                    newFootPosition += (m_leftFootTarget.position - newFootPosition).normalized * 0.02f;
+                }
+
+            }
+
         }
 
-        m_rightFootTarget.position = m_targetRightFootPosition;
-        m_leftFootTarget.position = m_targetLeftFootPosition;
+
         /*m_rightFootTarget.position = Vector3.Lerp(m_targetRightFootPosition,
                 m_rightFootTarget.position,
                 Time.deltaTime * m_footMovementSmoothValue);
@@ -122,6 +119,39 @@ public class IKAnimator : MonoBehaviour
             m_leftFootTarget.position,
             Time.deltaTime * m_footMovementSmoothValue);*/
     }
+
+    private void BalanceFeet()
+    {
+
+    }
+
+    private bool MoveRightFoot(Vector3 newRightFootPosition)
+    {
+        Debug.Log("MOVE RIGHT");
+        if (Physics.Raycast(newRightFootPosition + Vector3.up * 1.5f,
+            Vector3.down,
+            out RaycastHit hitInfo,
+            2f))
+        {
+            m_rightFootTarget.position = hitInfo.point;
+            return true;
+        }
+        return false;
+    }
+    private bool MoveLeftFoot(Vector3 newLeftFootPosition)
+    {
+        Debug.Log("MOVE RIGHT");
+        if (Physics.Raycast(newLeftFootPosition + Vector3.up * 1.5f,
+            Vector3.down,
+            out RaycastHit hitInfo,
+            2f))
+        {
+            m_leftFootTarget.position = hitInfo.point;
+            return true;
+        }
+        return false;
+    }
+
 
     private void OnDrawGizmosSelected()
     {
