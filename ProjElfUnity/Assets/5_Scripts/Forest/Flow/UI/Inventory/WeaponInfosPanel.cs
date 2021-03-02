23d04c396ae7815a3d69675209c78fc46fb88f38
Gretsok <47755843+Gretsok@users.noreply.github.com>
2021-03-02 17:56:45 +0100

    Inflate correctly weapon infos panel

--- ProjElfUnity/Assets/5_Scripts/Forest/Flow/UI/Inventory/WeaponInfosPanel.cs
+++ ProjElfUnity/Assets/5_Scripts/Forest/Flow/UI/Inventory/WeaponInfosPanel.cs
@@ -14,20 +14,43 @@ namespace ProjElf.HubForest
         public void InflateMeleeWeapon(MeleeWeaponData.MeleeWeaponSaveData weaponSaveData)
         {
             InflateBasicInfos(weaponSaveData);
+
+            for(int i = 3; i < m_statsTextLines.Length; i++)
+            {
+                m_statsTextLines[i].gameObject.SetActive(false);
+            }
         }
 
         public void InflateBow(BowData.BowSaveData weaponSaveData)
         {
             InflateBasicInfos(weaponSaveData);
+
+            m_statsTextLines[3].text = $"Gravity applied to projectile: {weaponSaveData.ProjectileDivingRate} m/s²";
+            m_statsTextLines[4].text = $"Projectile speed: {weaponSaveData.ProjectileSpeed} s";
+
+            for (int i = 5; i < m_statsTextLines.Length; i++)
+            {
+                m_statsTextLines[i].gameObject.SetActive(false);
+            }
         }
 
         public void InflateGrimoire(GrimoireData.GrimoireSaveData weaponSaveData)
         {
             InflateBasicInfos(weaponSaveData);
+            m_statsTextLines[3].text = $"Projectile speed: {weaponSaveData.ProjectileSpeed} m/s";
+            m_statsTextLines[4].text = $"Projectile life time: {weaponSaveData.ProjectileLifeTime} s";
+            for (int i = 5; i < m_statsTextLines.Length; i++)
+            {
+                m_statsTextLines[i].gameObject.SetActive(false);
+            }
         }
 
         private void InflateBasicInfos(AWeaponData.AWeaponSaveData weaponSaveData)
         {
+            for(int i = 0; i < m_statsTextLines.Length; i++)
+            {
+                m_statsTextLines[i].gameObject.SetActive(true);
+            }
             m_weaponNameText.text = weaponSaveData.WeaponName;
             m_statsTextLines[0].text = "Attack Speed: " + weaponSaveData.AttackSpeed;
             m_statsTextLines[1].text =  $"{weaponSaveData.HitDamage.HitDamage} of {weaponSaveData.HitDamage.DamageType.ToString()} Damage";
