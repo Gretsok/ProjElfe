4f54fbd4d108b02bb067b9abaf6b9ea005dda478
Fergal MECHIN <47755843+Gretsok@users.noreply.github.com>
2021-05-04 18:02:06 +0200

    Adventure Portal

---
+++ ProjElfUnity/Assets/5_Scripts/Forest/Interactable/AdventurePortal.cs
@@ -0,0 +1,34 @@
+﻿using ProjElf.Interaction;
+using UnityEngine;
+
+namespace ProjElf.HubForest
+{
+    public class AdventurePortal : MonoBehaviour, IInteractable
+    {
+        private HubForestGameMode m_gamemode = null;
+        private void Start()
+        {
+            m_gamemode = MOtter.MOtterApplication.GetInstance().GAMEMANAGER.GetCurrentMainStateMachine<HubForestGameMode>();
+        }
+        public void DoInteraction(Interactor interactor = null)
+        {
+            Debug.Log("Interact with portal");
+            
+        }
+
+        public void StartBeingWatched()
+        {
+            Debug.Log("Portal start being watched");
+        }
+
+        public void StopBeingWatched()
+        {
+            Debug.Log("Portal stop being watched");
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            m_gamemode.ActivateDunjeonSelectionState();
+        }
+    }
+}
