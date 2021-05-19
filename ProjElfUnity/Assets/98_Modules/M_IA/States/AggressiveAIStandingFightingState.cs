f6019e6f0863e6e3bd5b784e14cf5ce7c985dc08
JulienBergeon <55741381+JulienBergeon@users.noreply.github.com>
2021-05-19 19:09:04 +0200

    Fait pas le malin parce que t'as trouvé l'erreur

--- ProjElfUnity/Assets/98_Modules/M_IA/States/AggressiveAIStandingFightingState.cs
+++ ProjElfUnity/Assets/98_Modules/M_IA/States/AggressiveAIStandingFightingState.cs
@@ -19,7 +19,7 @@ public class AggressiveAIStandingFightingState : GenericAIState
     {
         base.EnterState();
         m_owner.Agent.SetDestination(m_owner.transform.position);
-        (m_owner as AggressiveAI).CombatController.StartUseWeapon(m_owner.Player.transform.forward);
+        (m_owner as AggressiveAI).CombatController.StartUseWeapon(m_owner.transform.forward);
     }
 
     public override void LateUpdateState()
