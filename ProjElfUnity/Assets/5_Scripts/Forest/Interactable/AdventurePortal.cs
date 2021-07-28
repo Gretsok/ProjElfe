1cc8e76f862263523a285ad51d147fbbe2932549
Gretsok <47755843+Gretsok@users.noreply.github.com>
2021-07-28 15:05:57 +0200

    MainMenu - import skybox + bake lights

--- ProjElfUnity/Assets/5_Scripts/Forest/Interactable/AdventurePortal.cs
+++ ProjElfUnity/Assets/5_Scripts/Forest/Interactable/AdventurePortal.cs
@@ -6,9 +6,19 @@ namespace ProjElf.HubForest
     public class AdventurePortal : MonoBehaviour, IInteractable
     {
         private HubForestGameMode m_gamemode = null;
+
+        #region Sounds refs
+        [SerializeField]
+        private MOtter.SoundManagement.SoundData m_portalSoundData = null;
+        [SerializeField]
+        private float m_soundHeightPosition = 2f;
+        private AudioSource m_portalAudioSource = null;
+        #endregion
+
         private void Start()
         {
             m_gamemode = MOtter.MOtterApplication.GetInstance().GAMEMANAGER.GetCurrentMainStateMachine<HubForestGameMode>();
+            m_portalAudioSource = MOtter.MOtterApplication.GetInstance().SOUND.Play3DSound(m_portalSoundData, transform.position + Vector3.up * 2, true, 10f);
         }
         public void DoInteraction(Interactor interactor = null)
         {
@@ -30,6 +40,24 @@ namespace ProjElf.HubForest
         {
             if(other.gameObject == m_gamemode.Player.gameObject)
                 m_gamemode.ActivateDunjeonSelectionState();
+        }
+
+
+#if UNITY_EDITOR
+        private void OnValidate()
+        {
+            if (m_portalAudioSource != null && m_portalAudioSource.isPlaying)
+            {
+                m_portalAudioSource.transform.position = transform.position + Vector3.up * m_soundHeightPosition;
+            }
+        }
+#endif
+        private void OnDestroy()
+        {
+            if(m_portalAudioSource != null && m_portalAudioSource.isPlaying)
+            {
+                m_portalAudioSource.Stop();
+            }
         }
     }
 }
